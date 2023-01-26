9f64395c0d1a8ac8fa7d6ab82f6ca37346058e55
ANJALI KAVADE <kavadeanjali@gamil.com>
2023-01-26 22:55:40 +0530

    added aware agriculture project

---
+++ aware.js
@@ -0,0 +1,13 @@
+const circle1 = document.getElementById("circle1");
+const circle2 = document.getElementById("circle2");
+const circle3 = document.getElementById("circle3");
+
+circle1.addEventListener('click', () => {
+  window.location.href = 'https://www.google.com';
+});
+circle2.addEventListener('click', () => {
+  window.location.href = 'storage1.html';
+});
+circle3.addEventListener('click', () => {
+  window.location.href = 'volform1.html';
+});
---
+++ volform.js
@@ -0,0 +1,7 @@
+let submitButton = document.getElementById("submit-button");
+
+// Add a click event listener to the submit button
+submitButton.addEventListener("click", function() {
+  // Show the message
+  alert("Thank you for submitting the form!");
+});
